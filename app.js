ea4af26f285dafd0262b3a2620f0e08e4965efe9
Sruthilaya <sruthilaya.vss@gmail.com>
2020-10-21 19:52:08 +0530

    stored the post details in a js object in post method

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.get("/compose",function(req,res){
 
 //console log the postTitle
 app.post("/compose",function(req,res){
-  console.log(req.body.postTitle);
+  const post={
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
 });
 
 
